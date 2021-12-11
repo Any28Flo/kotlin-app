57e42f9a11cb6b004306a59f162c1095db9ec6e8
Erika Flores <foglzerika@gmail.com>
2021-12-11 09:08:41 -0600

    feat | decare variables val and var

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,23 @@
+import kotlin.math.log
+
 fun main() {
-    println("Hello from kotlin")
+    println("Hello from kotlin");
+    // val -> Para valores no reutilizable
+
+    val curp = "FOGE821228MMCLRN09";
+    println(curp);
+    val isAlive = true;
+    println(isAlive);
+
+    //var -> Para palabras reutilizables
+
+    var isLoged = true;
+    println(isLoged);
+    var nombre:String = "Snoppy";
+    println(nombre);
+    var saludo = "Hola Snoppy";
+    println(saludo);
+
+
+
 }//main
